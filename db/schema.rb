5ac23bef4a3fe79e5a8cbe6bb42b1469f109eab4
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-01-23 21:56:42 -0800

    add email and password fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_24_052653) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
