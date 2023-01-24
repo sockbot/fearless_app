5f4626be79c00e4c3d6b546376244a6c7ef6f610
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-01-23 22:09:16 -0800

    add email validation on User model and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "is not valid without an email" do
+    it "has no email" do
+      user = User.new()
+      expect(user).to_not be_valid
+    end
+    it "has an email of nil" do
+      user = User.new(email: nil)
+      expect(user).to_not be_valid
+    end
+  end
+
+  it "is valid with an email" do
+    user = User.new(email: "asdf@asdf.com")
+    expect(user).to be_valid
+  end
+
 end
