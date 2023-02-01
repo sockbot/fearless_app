d62d7578bf76f2c0d3dd0ab0a6ce2aa924f6b6c8
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-01-31 21:32:37 -0800

    remove autoincluded defaults from User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
-class User < ApplicationRecord
-  include ActiveModel::SecurePassword
+class User < ApplicationRecord 
   has_secure_password
-  has_secure_password validations: true
 
   attr_accessor :password_digest
-
+  
   validates :email, presence: true
-  validates :password_digest, presence: true
 end
