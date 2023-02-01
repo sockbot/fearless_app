5872202cec2c02014d07bc40b335383de583f6df
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-01-31 21:33:09 -0800

    rewrite tests to only assert validity of User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  describe "is not valid without both email and password_digest" do
+  describe "is not valid without both email and password" do
     it "has no email" do
       user = User.new()
       expect(user).to_not be_valid
@@ -11,22 +11,22 @@ RSpec.describe User, type: :model do
       user = User.new(email: nil)
       expect(user).to_not be_valid
     end
-    it "has an email of nil with a password_digest" do
-      user = User.new(email: nil, password_digest: "Password1!")
+    it "has an email of nil with a password" do
+      user = User.new(email: nil, password: "Password1!")
       expect(user).to_not be_valid
     end
-    it "is not valid with an email but without a password_digest" do
+    it "is not valid with an email but without a password" do
       user = User.new(email: "asdf@asdf.com")
       expect(user).to_not be_valid
     end
-    it "has an email with a password_digest of nil" do
-      user = User.new(email: "email", password_digest: nil)
+    it "has an email with a password of nil" do
+      user = User.new(email: "email", password: nil)
       expect(user).to_not be_valid
     end
   end
 
   it "is valid with an email and password" do
-    user = User.new(email: "asdf@asdf.com", password_digest: "Password1!")
+    user = User.new(email: "asdf@asdf.com", password: "Password1!")
     expect(user).to be_valid
   end
 
