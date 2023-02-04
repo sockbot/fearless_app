01458c810f31e910ca9b09bf22b035fff550fa55
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-03 18:45:59 -0800

    create nested route for user profile#index and view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   resources :home, only: [:index]
-  resources :users, only: [:new, :create, :index]
+  resources :users, only: [:new, :create, :index] do
+    resources :profile, only: [:index]
+  end
 end
