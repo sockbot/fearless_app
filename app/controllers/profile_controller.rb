a75e56155691181be0db6fe43f45ba764ef0195b
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-03 22:30:15 -0800

    polish error handling and fix bug with save call

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -7,11 +7,11 @@ class ProfileController < ApplicationController
     @user = User.find(params[:user_id])
     @user.profile = Profile.new(profile_params)
 
-    if user.profile.save
+    if @user.profile.save
       redirect_to user_profile_index_path
     else
-      puts "Profile didn't save"
-      render :new
+      flash[:alert] = "Profile didn't save, try again"
+      render :new, status: :unprocessable_entity
     end
   end
 
