33d21c42aa87daf964640db2af2703d8b91ed6b6
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-13 21:43:01 -0800

    implement session create (login)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   resources :home, only: [:index]
+  get 'signup' => 'users#new'
   resources :users, only: [:new, :create, :index, :show] do
     resource :profile, only: [:new, :create, :show]
   end
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  # delete 'logout'  => 'sessions#destroy'
+  resources :sessions, only: [:new, :create] 
 end
