bc6424d5ab05d531ad5db35ff590539209adaa9f
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-13 23:29:19 -0800

    add sad path tests for creating sessions

--- spec/requests/session_requests_spec.rb
+++ spec/requests/session_requests_spec.rb
@@ -11,10 +11,25 @@ RSpec.describe "Session", type: :request do
   end
 
   describe "POST /sessions" do
+    before(:all) do
+      @user = User.new(email: "hello@world.com", password: "Password1!")
+    end
+
     it "creates a new session with valid email and password" do
-      user = User.new(email: "hello@world.com", password: "Password1!")
       post sessions_url, params: {user: {email: "hello@world.com", password: "Password1!"}}
-      expect(session[:user_id]).to eq(user.id)
+      expect(session[:user_id]).to eq(@user.id)
+    end
+
+    it "fails to create a new session with invalid email" do
+      user = User.new(email: "hello@world.com", password: "Password1!")
+      post sessions_url, params: {user: {email: "notanemail", password: "Password1!"}}
+      expect(session[:user_id]).to eq(nil)
+    end
+
+    it "fails to create a new session with invalid password" do
+      user = User.new(email: "hello@world.com", password: "Password1!")
+      post sessions_url, params: {user: {email: "hello@world.com", password: nil}}
+      expect(session[:user_id]).to eq(nil)
     end
   end
 end
