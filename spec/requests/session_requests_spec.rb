9fa2250c315f4ac807f5c4aa227cd43eb1255581
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-15 18:55:46 -0800

    use aliases in session spec routes

--- spec/requests/session_requests_spec.rb
+++ spec/requests/session_requests_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Session", type: :request do
   
-  describe "GET /sessions/new" do
+  describe "GET /login" do
     it "renders the login template" do
-      get new_session_path
+      get login_path
       expect(response).to have_http_status(200)
       expect(response.body).to include("Log In")
     end 
@@ -16,19 +16,19 @@ RSpec.describe "Session", type: :request do
     end
 
     it "creates a new session with valid email and password" do
-      post sessions_url, params: {user: {email: "hello@world.com", password: "Password1!"}}
+      post sessions_path, params: {user: {email: "hello@world.com", password: "Password1!"}}
       expect(session[:user_id]).to eq(@user.id)
     end
 
     it "fails to create a new session with invalid email" do
       user = User.new(email: "hello@world.com", password: "Password1!")
-      post sessions_url, params: {user: {email: "notanemail", password: "Password1!"}}
+      post sessions_path, params: {user: {email: "notanemail", password: "Password1!"}}
       expect(session[:user_id]).to eq(nil)
     end
 
     it "fails to create a new session with invalid password" do
       user = User.new(email: "hello@world.com", password: "Password1!")
-      post sessions_url, params: {user: {email: "hello@world.com", password: nil}}
+      post sessions_path, params: {user: {email: "hello@world.com", password: nil}}
       expect(session[:user_id]).to eq(nil)
     end
   end
