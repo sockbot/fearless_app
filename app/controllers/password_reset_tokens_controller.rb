0e7d5b981731862af5c12c811d616ca74d885de7
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-02-27 19:26:36 -0800

    implement aws ses email with reset token

--- app/controllers/password_reset_tokens_controller.rb
+++ app/controllers/password_reset_tokens_controller.rb
@@ -10,10 +10,43 @@ class PasswordResetTokensController < ApplicationController
     user.regenerate_password_reset_token
     if user.save
       flash[:notice] = "New password reset token created."
+      send_password_reset_email(user)
       redirect_to login_path
     else
       flash[:alert] = "Couldn't regenerate password reset token. Try again"
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def send_password_reset_email(user)
+    client = Aws::SES::Client.new(
+      region: "us-west-2", 
+      access_key_id: Rails.application.credentials[:aws][:access_key_id], 
+      secret_access_key: Rails.application.credentials[:aws][:secret_access_key]
+    )
+      
+    begin
+      client.send_email(
+        destination: {
+          to_addresses: [user.email]
+        },
+        message: {
+          body: {
+            text: {
+              data: "Your password reset token is #{user.password_reset_token}"
+            }
+          },
+          subject: {
+            data: "DM Blog - Reset your password"
+          }
+        },
+        source: "dmblog@sockbot.com",
+      )
+      puts "Email sent to #{user.email}"
+    rescue Aws::SES::Errors::ServiceError => error
+      puts "Email not sent. Error message: #{error}"
+    end  
   end
 end
