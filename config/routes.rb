a0339a2fe8f84b6987b129635eaec39e25fbab0f
Andy Chan <1291473+sockbot@users.noreply.github.com>
2023-04-26 18:35:02 -0700

    add search page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   get 'logout'  => 'sessions#destroy'
   get 'signup' => 'users#new'
-
+  get 'search' => 'pages#search'
+  
   resources :home, only: [:index]
   resource :profile, only: [:new, :create, :show]
   resolve("Profile") { [:profile] }
